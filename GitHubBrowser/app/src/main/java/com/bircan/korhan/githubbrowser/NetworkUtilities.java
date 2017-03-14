43ce3996f53fb48153e020dcc33b162786f82040
Korhan Bircan <korhan.bircan@capitalone.com>
2017-03-13 22:16:35 -0700

    Added method to return http response as a string.

--- GitHubBrowser/app/src/main/java/com/bircan/korhan/githubbrowser/NetworkUtilities.java
+++ GitHubBrowser/app/src/main/java/com/bircan/korhan/githubbrowser/NetworkUtilities.java
@@ -2,8 +2,12 @@ package com.bircan.korhan.githubbrowser;
 
 import android.net.Uri;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
  * Created by ftq194 on 3/13/17.
@@ -15,6 +19,11 @@ public class NetworkUtilities {
     final static String PARAM_SORT = "sort";
     final static String SORTY_BY = "stars";
 
+    /**
+     * Method that returns a search URL with the given keyword.
+     * @param keyword Search keyword.
+     * @return GitHub search URL with the given keyword.
+     */
     public static URL searchURL(String keyword) {
         Uri searchURI = Uri.parse(GITHUB_BASE_URL).buildUpon()
                 .appendQueryParameter(PARAM_QUERY, keyword)
@@ -29,5 +38,29 @@ public class NetworkUtilities {
         }
 
         return url;
+    }
+
+    /**
+     * Method to return HTTP response as a string.
+     * @param url The URL to get the HTTP response from.
+     * @return The HTTP response as a string.
+     * @throws IOException
+     */
+    public static String stringResponseFrom(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+        try {
+            InputStream inputStream = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(inputStream);
+            scanner.useDelimiter("\\A");
+
+            boolean hasNext = scanner.hasNext();
+            if (hasNext) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
